c6fd555642c7b414d219cc34ea21f65b808a88c5
JerryGuo2001 <jianleg@uci.edu>
2025-05-01 02:04:59 -0700

    update header

--- task.js
+++ task.js
@@ -427,7 +427,7 @@ function formatPositionsForCSV(posObj) {
 
 
 function saveCSV() {
-    const header = 'id,trial,type,rt,choice,block_a,node_count_a,block_b,node_count_b,graphA,graphB,pc1_A,pc2_A,pc1_B,pc2_B,posA,posB';
+    const header = 'id,trial,type,rt,choice,block_a,node_count_a,block_b,node_count_b,graphA,graphB,pc1_A,pc3_A,pc1_B,pc3_B,posA,posB';
     const rows = trialData.map(row => {
         return `${row.id},${row.trial},${row.type},${row.rt},${row.choice},${row.block_a},${row.node_count_a},${row.block_b},${row.node_count_b},"${JSON.stringify(row.graphA)}","${JSON.stringify(row.graphB)}",${row.pc1_A},${row.pc2_A},${row.pc1_B},${row.pc2_B},"${row.posA}","${row.posB}"`;
     });
